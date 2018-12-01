ec0f78d502cd80e311a6821fd0cbd461223ffbe3
ValBarraza <valentina.barraza@davinci.edu.ar>
2018-12-01 01:03:14 -0300

    Level

---
+++ Assets/Scripts/Characther/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * 10 * Time.deltaTime;
+	}
+	void OnCollisionEnter(Collision c){
+		//Enemy _enemy = c.GetComponent<Enemy>();
+		//if(_enemy) _enemy.hp --;
+	}
+}
---
+++ Assets/Scripts/Characther/Characther.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characther : MonoBehaviour {
+
+	public GameObject _bulletPrefab;
+	void Start () {
+		
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q)) Shoot();
+	}
+	void Shoot(){
+		GameObject bullet = Instantiate(_bulletPrefab) as GameObject;
+		bullet.transform.position = this.transform.position;
+		bullet.transform.forward = this.transform.forward;
+	}
+}
