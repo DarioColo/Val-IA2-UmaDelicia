fb174f4d9c72b35645cc69b3a67d3a83178a00d6
DarioColo <32600360+DarioColo@users.noreply.github.com>
2018-12-01 00:00:30 -0300

    Inicio

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public void Patrol() { }
+    public void Alert() { }
+    public void Chase() { }
+    public void Attack() { }
+}
---
+++ Assets/Scripts/EventController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventController : MonoBehaviour {
+
+    public event Action<Event> OnEvent = delegate { };
+
+	void Update () {
+		//if(booleanos)
+	}
+}
+public enum Event
+{
+    onPatrol,
+    offPatrol,
+    onChase,
+    offChase,
+    onAlert,
+    offAlert,
+    onAttack,
+    offAttack
+}
